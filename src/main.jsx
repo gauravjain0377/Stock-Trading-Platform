72c0c68070e96e5d7a29a6d98169557167aeda97
Gaurav Jain <jaingaurav906@gmail.com>
2025-04-13 22:36:05 +0530

    Home-Page-Structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HomePage from './landing_page/home/HomePage';
+import HomePage from './landing_page/home/HomePage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
