2db2b43a43c5e52b61ef943bc42f5aa1ac4dae48
Gaurav Jain <jaingaurav906@gmail.com>
2025-04-14 11:15:24 +0530

    Pricing

--- src/landing_page/home/Pricing.jsx
+++ src/landing_page/home/Pricing.jsx
@@ -5,12 +5,12 @@ function Pricing() {
   <div className='container'>
     <div className='row'>
       <div className='col-4'>
-        <h1 className='mb-3'>Unbeatable pricing</h1>
+        <h1 className='mb-3 fs-2'>Unbeatable pricing</h1>
         <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
         <a href="" style={{textDecoration: "none"}}>See Pricing <i class="fa-solid fa-arrow-right-long" aria-hidden="true"></i></a>
       </div>
       <div className='col-2'></div>
-      <div className='col-6'>
+      <div className='col-6 mb-5'>
         <div className='row text-center'>
 
           <div className='col p-3 border'>
