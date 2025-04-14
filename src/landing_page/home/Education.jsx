13099fd251da0e980ff08e614cd3ac55c49697a4
Gaurav Jain <jaingaurav906@gmail.com>
2025-04-14 14:17:59 +0530

    Home Page Done

--- src/landing_page/home/Education.jsx
+++ src/landing_page/home/Education.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 
+
 function Education() {
   return (
     <div className='container mt-5'>
     <div className='row'>
       <div className='col-6'>
-        <img src="media/images/education.svg" />
+        <img style={{width: "70%"}} src="media/images/education.svg" />
 
       </div>
-
-      <div className='col-6'>
-        <h1 className='fs-2'>Free and open market education</h1>
+      
+      <div className='col-6 mt-5'>
+        <h1 className='fs-2 mb-4'>Free and open market education</h1>
         <p>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
-        <a href="" className='mx-5' style={{textDecoration: "none"}}>Varsity <i class="fa-solid fa-arrow-right-long"></i></a>
-        <p className='mt-5'>TradingQ&A, the most active trading and investment community in India for all your market related queries.</p>
+        <a href=""  style={{textDecoration: "none"}}>Varsity <i class="fa-solid fa-arrow-right-long"></i></a>
+        <p className='mt-3'>TradingQ&A, the most active trading and investment community in India for all your market related queries.</p>
 
-        <a href="" className='mx-5' style={{textDecoration: "none"}}>TadingQ&A <i class="fa-solid fa-arrow-right-long"></i></a>
+        <a href="" style={{textDecoration: "none"}}>TadingQ&A <i class="fa-solid fa-arrow-right-long"></i></a>
       </div>
       
 
